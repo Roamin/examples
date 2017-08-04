51d5b15ba3636cf63cc7a4cbbce479af8f7d27ca
Pulsating <ivill@live.com>
2017-08-04 23:00:21 +0800

    do than never

--- Core_HTML5_Canvas_Graphics_Animation_and_Game_Development/Chapter 1/1-9/src/js/example.js
+++ Core_HTML5_Canvas_Graphics_Animation_and_Game_Development/Chapter 1/1-9/src/js/example.js
@@ -1,1 +1,12 @@
 const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const rubberBandDiv = document.getElementById('rubberBandDiv');
+const resetButton = document.getElementById('resetButton');
+
+const image = new Image();
+const mouseDown = {};
+const rubberBandRectAngle = {};
+
+let dragging = false;
+
