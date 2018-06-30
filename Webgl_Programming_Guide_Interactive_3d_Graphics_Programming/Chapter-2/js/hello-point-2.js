b5056689dc6bbd3af58b83d3478bc440e9accf0e
RoamIn <ivill@live.com>
2018-06-30 22:43:20 +0800

    use attribute

---
+++ Webgl_Programming_Guide_Interactive_3d_Graphics_Programming/Chapter-2/js/hello-point-2.js
@@ -0,0 +1,58 @@
+// 顶点着色器程序
+// Vertex shade（顶点着色器）：用来描述顶点特性（如位置、颜色等）
+// 顶点是指二维或三维空间中的一个点，比如二维或者三维图像的端点或交点
+const VSHADER_SOURCE = `
+    attribute vec4 a_Position;
+    void main () {
+        gl_Position = a_Position; // 设置坐标
+        gl_PointSize = 10.0; // 设置尺寸
+    }
+`
+
+// 片元着色器程序
+// Fragment shader（片元着色器）：进行逐片元处理过程，如光照
+const FSHADER_SOURCE = `
+    void main () {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // 设置颜色
+    }
+`
+
+
+function main () {
+    const canvas = document.getElementById('webgl')
+    const gl = getWebGLContext(canvas)
+
+    if (!gl) {
+        console.log('Failed to get the rendering context for ThreeJS.')
+        
+        return
+    }
+
+    // 初始化着色器
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.')
+
+        return
+    }
+
+    // 获取 attribute 变量的存储位置
+    const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position')
+
+        return
+    }
+
+    // 将顶点位置传输给 attribute 变量
+    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0)
+
+    // 设置 canvas 的背景色
+    gl.clearColor(0.0, 0.0, 0.0, 1.0)
+
+    // 清空 canvas
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // 绘制一个点
+    gl.drawArrays(gl.POINTS, 0, 1)
+}
